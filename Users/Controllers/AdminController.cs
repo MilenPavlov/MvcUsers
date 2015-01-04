a4506e7ad5a43c24179377e842f44daca4bada39
MilenPavlov <milenppavlov@gmail.com>
2015-01-04 18:12:44 +0000

    create user

--- Users/Controllers/AdminController.cs
+++ Users/Controllers/AdminController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Users.Infrastructure;
+using Users.Models;
 
 namespace Users.Controllers
 {
@@ -15,6 +17,40 @@ namespace Users.Controllers
         public ActionResult Index()
         {
             return View(UserManager.Users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser {UserName = model.Name, Email = model.Email};
+                var result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
         }
 
         private AppUserManager UserManager
